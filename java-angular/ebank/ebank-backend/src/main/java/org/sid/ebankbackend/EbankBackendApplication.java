95b8ea5adabf1e52bbd1f9884c5f68a2a8baeb87
bassiroutdsi <bassiroutds@gmail.com>
2024-05-30 01:31:02 +0200

    Swagger not done

--- java-angular/ebank/ebank-backend/src/main/java/org/sid/ebankbackend/EbankBackendApplication.java
+++ java-angular/ebank/ebank-backend/src/main/java/org/sid/ebankbackend/EbankBackendApplication.java
@@ -1,5 +1,7 @@
 package org.sid.ebankbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.sid.ebankbackend.dtos.CustomerDTO;
 import org.sid.ebankbackend.entities.*;
 import org.sid.ebankbackend.enums.AccountStatus;

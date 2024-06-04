49de897b6a11b5ad9025f16b4ff63363423f496e
bassiroutdsi <bassiroutds@gmail.com>
2024-06-04 23:36:25 +0200

    config-server and discory-server configured

---
+++ java-angular/e-commerce-app/services/discovery/src/test/java/koula/diallo/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package koula.diallo.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

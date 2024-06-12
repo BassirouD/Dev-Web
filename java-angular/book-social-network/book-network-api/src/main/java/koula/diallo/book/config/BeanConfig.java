6932241954f87e3b3dc11baba094acf605336086
bassiroutdsi <bassiroutds@gmail.com>
2024-06-12 03:58:23 +0200

    Security configured

---
+++ java-angular/book-social-network/book-network-api/src/main/java/koula/diallo/book/config/BeanConfig.java
@@ -0,0 +1,28 @@
+package koula.diallo.book.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeanConfig {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncode());
+        return authenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncode() {
+        return new BCryptPasswordEncoder();
+    }
+}

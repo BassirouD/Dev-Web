ef898b9a884844b72e5950ccb25a9069f17a9a1f
bassiroutdsi <bassiroutds@gmail.com>
2024-06-14 02:37:24 +0200

    save token added after login method

--- java-angular/book-social-network/book-network-ui/src/app/pages/login/login.component.ts
+++ java-angular/book-social-network/book-network-ui/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,13 @@ import {AuthenticationService} from "../../services/services/authentication.serv
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  authRequest: AuthenticationRequest = {email: '', password: ''}
+  authRequest: AuthenticationRequest = {email: 'tata@book-social.com', password: 'password11'}
   errMsg: Array<string> = [];
 
   constructor(private router: Router,
-              private authSrv: AuthenticationService) {
+              private authSrv: AuthenticationService,
+              private tokenSrv: TokenService
+  ) {
   }
 
   login() {
@@ -22,11 +25,16 @@ export class LoginComponent {
       body: this.authRequest
     }).subscribe({
       next: (res) => {
-        //todo save token
+        this.tokenSrv.token = res.token as string
         this.router.navigate(['books'])
       },
       error: (err) => {
         console.log(err)
+        if (err.error.validationErrors) {
+          this.errMsg = err.error.validationErrors
+        } else {
+          this.errMsg.push(err.error.error)
+        }
       }
     })
   }

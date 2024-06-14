d2aeed21faa1e8a4403791c75c80cb50f44d929d
bassiroutdsi <bassiroutds@gmail.com>
2024-06-14 20:15:07 +0200

    pagination(book-list) implemented

--- java-angular/book-social-network/book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ java-angular/book-social-network/book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -4,32 +4,67 @@ import {Router} from "@angular/router";
 import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
 
 @Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.scss'
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit {
-  bookResponse: PageResponseBookResponse = {};
-  page: number = 0;
-  size: number = 5;
+    bookResponse: PageResponseBookResponse = {};
+    page: number = 0;
+    size: number = 2;
 
 
-  ngOnInit(): void {
-    this.findAllBooks()
-  }
+    ngOnInit(): void {
+        this.findAllBooks()
+    }
 
 
-  constructor(private bookSrv: BookService, private router: Router) {
-  }
+    constructor(private bookSrv: BookService, private router: Router) {
+    }
 
-  private findAllBooks() {
-    this.bookSrv.findAllBooks({
-      page: this.page,
-      size: this.size,
-    }).subscribe({
-      next: (books) => {
-        this.bookResponse = books
-      }
-    })
-  }
+    private findAllBooks() {
+        this.bookSrv.findAllBooks({
+            page: this.page,
+            size: this.size,
+        }).subscribe({
+            next: (books) => {
+                this.bookResponse = books
+            }
+        })
+    }
+
+    goToFirstPage() {
+        this.page = 0;
+        this.findAllBooks();
+    }
+
+    goToPreviousPage() {
+        this.page--;
+        this.findAllBooks();
+    }
+
+    goToPage(page: number) {
+        this.page = page
+        this.findAllBooks();
+    }
+
+    goToNextPage() {
+        this.page++;
+        this.findAllBooks();
+    }
+
+    goToLastPage() {
+        this.page = this.bookResponse.totalPages as number - 1;
+        this.findAllBooks();
+    }
+
+    get isLastPage(): boolean {
+        return this.page == this.bookResponse.totalPages as number - 1;
+    }
+
+    get totalPages(): any[] {
+        // On peut utiliser directement cette methodes pour avoir le totalPages
+        // Pour ne pas à faire [].constructor(bookResponse.totalPages) dans le html
+        return new Array(this.bookResponse.totalPages);
+    }
 }

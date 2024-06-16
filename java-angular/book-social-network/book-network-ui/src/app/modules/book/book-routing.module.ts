a49031ab7b804909988a9c09ce7a084f63aaad97
bassiroutdsi <bassiroutds@gmail.com>
2024-06-16 13:31:28 +0200

    guard implemented

--- java-angular/book-social-network/book-network-ui/src/app/modules/book/book-routing.module.ts
+++ java-angular/book-social-network/book-network-ui/src/app/modules/book/book-routing.module.ts
@@ -6,18 +6,20 @@ import {MyBookComponent} from "./pages/my-book/my-book.component";
 import {ManageBookComponent} from "./pages/manage-book/manage-book.component";
 import {BorrowedBookListComponent} from "./pages/borrowed-book-list/borrowed-book-list.component";
 import {ReturnBooksComponent} from "./pages/return-books/return-books.component";
+import {authGuard} from "../../services/guard/auth.guard";
 
 const routes: Routes = [
     {
         path: '',
         component: MainComponent,
+        canActivate: [authGuard],
         children: [
-            {path: '', component: BookListComponent},
-            {path: 'my-books', component: MyBookComponent},
-            {path: 'manage', component: ManageBookComponent},
-            {path: 'manage/:bookId', component: ManageBookComponent},
-            {path: 'my-borrowed-books', component: BorrowedBookListComponent},
-            {path: 'my-return-books', component: ReturnBooksComponent},
+            {path: '', component: BookListComponent, canActivate: [authGuard],},
+            {path: 'my-books', component: MyBookComponent, canActivate: [authGuard],},
+            {path: 'manage', component: ManageBookComponent, canActivate: [authGuard],},
+            {path: 'manage/:bookId', component: ManageBookComponent, canActivate: [authGuard],},
+            {path: 'my-borrowed-books', component: BorrowedBookListComponent, canActivate: [authGuard],},
+            {path: 'my-return-books', component: ReturnBooksComponent, canActivate: [authGuard],},
         ]
     }
 ];
